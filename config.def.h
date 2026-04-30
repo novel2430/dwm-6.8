4d74eb9a51ed68baf5a6d6bb6a1f75e59ce8274d
novel2430 <novel2430@163.com>
2026-05-01 00:58:20 +0800

    update

--- config.def.h
+++ config.def.h
@@ -84,14 +84,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] =
     "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = {"rofi", "-show", "drun", NULL};
-static const char *termcmd[] = {"tabbed",
-                                "-c",
-                                "-r",
-                                "2",
-                                "st",
-                                "-w"
-                                "''",
-                                NULL};
+static const char *termcmd[] = {"tabbed-st", NULL};
 static const char *runmenucmd[] = {"rofi", "-show", "run", NULL};
 
 static const Key keys[] = {
